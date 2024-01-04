6393059dbe396121dc51a4ba021e0e8620372fc7
engh0205@naver.com <engh0205@naver.com>
2024-01-04 17:37:53 +0900

    Update Readme

--- stock/src/main/java/api/stock/stock/global/filter/JwtAuthenticationFilter.java
+++ stock/src/main/java/api/stock/stock/global/filter/JwtAuthenticationFilter.java
@@ -37,7 +37,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         String token = parseBearerToken(request);
         SetOperations<String,String> setOperations = redisTemplate.opsForSet();
         boolean isLogout = false;
