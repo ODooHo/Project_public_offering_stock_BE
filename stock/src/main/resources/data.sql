cb050eedf6e4d7c674979d725cfa230295eb91c6
engh0205@naver.com <engh0205@naver.com>
2024-03-04 16:47:39 +0900

    test

---
+++ stock/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+insert into user(user_email, user_nickname, user_password, user_phone_number, user_profile) values
+                ('dooho','두호','1234','010-6637-8632','default.jpg');
+
+
+insert into board(board_id, board_write_date, click_count, comment_count, like_count, board_content, board_image, board_title, user_email) values
+                (1,'2024-01-30',0,0,0,'content','default.jpg','title','dooho');
